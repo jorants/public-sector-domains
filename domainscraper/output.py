434f86effbce1d295a56dceed63aba0243621b3c
Joran van Apeldoorn <joran@bitsoffreedom.nl>
2023-05-14 13:31:21 +0200

    fixed name in readme

--- domainscraper/output.py
+++ domainscraper/output.py
@@ -28,11 +28,11 @@ def markdown_results():
     )
 
 
-README = """# Public Institutes Domain Scraper
+README = """# Public Sector Domains - Results
 
 **This repository is temporarly hosted here, it will likely be moved in the future**
 
-This is the *result* branch of this project, check out the [main branch](https://github.com/jorants/public-institutes-domain-scraper/tree/main) for the underlying code.
+This is the *result* branch of this project, check out the [main branch](https://github.com/jorants/public-sector-domains) for the underlying code.
 
 """
 
